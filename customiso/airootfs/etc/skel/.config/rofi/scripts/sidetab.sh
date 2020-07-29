2b02c2cb43b1c24caffda19f03eaeb04e83a7eb3
adi1090x <adi1090x@gmail.com>
2020-07-30 00:25:52 +0530

    Files Removed

--- customiso/airootfs/etc/skel/.config/rofi/scripts/sidetab.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-rofi -no-lazy-grab -show drun \
--display-drun "Applications " -drun-display-format "{name}" \
--hide-scrollbar true \
--show-icons -icon-theme "Papirus" \
--theme ~/.config/rofi/themes/sidetab.rasi \
